c651db56350280614b29ffecbbdd07e6bd566739
Ilya Litik <runodj@gmail.com>
2020-02-15 13:59:29 +0300

    Init bot

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, Пашок (｡◕‿◕｡)')
+
+
+bot.polling()
